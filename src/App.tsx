9c4a16da3b2ffe2fe006895f7c4d8ae062244e91
lu4ezar <misha7shisha@gmail.com>
2021-03-05 07:58:22 +0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import Header from "./Header";
 import Router from "./Router";
+import Footer from "./Footer";
 import "./styles.scss";
 
 export default function App() {
   return (
-    <>
-      <div className="App">
-        <Header />
+    <div className="App">
+      <Header />
+      <div
+        style={{
+          display: "flex"
+        }}
+      >
         <Router />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <div
+    style={{
+      height: "10vh",
+      opacity: 0.7,
+      background: "#444444",
+      alignSelf: "flex-end"
+    }}
+  ></div>
+);
+
+export default Footer;
